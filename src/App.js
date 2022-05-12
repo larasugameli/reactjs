966975a7eb097cbe15dc771b53d6ed46980401f2
Lara Sugameli <lara.sugameli@gmail.com>
2022-05-12 15:08:40 -0700

    Segundo desafio

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+
+function App() {
+  return (
+    //JSX
+
+    <div className="navbar">
+      <NavBar />
+      Contenido del app
+    </div>
+  );
+}
+
+export default App;
