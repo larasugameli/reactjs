2da2e145110eb93cdb0a1721c0e8cca8c73adab1
Lara Sugameli <lara.sugameli@gmail.com>
2022-05-26 17:23:13 -0700

    Reentrega Desafio

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,10 +1,64 @@
+import { useState, useEffect } from "react";
 import ItemList from "../ItemList/ItemList";
 
-const ItemListContainer = ({ title, products }) => {
+const ItemListContainer = ({ title }) => {
+  const [products, setProducts] = useState([]);
+
+  const productos = [
+    {
+      image: "ClasicoNegro.png",
+      category: "Basic",
+      name: "Clásico Negro",
+      price: 400,
+      stock: 20,
+      id: 1,
+    },
+    {
+      image: "Paltas.png",
+      category: "Morfi",
+      name: "Paltas",
+      price: 400,
+      stock: 15,
+      id: 2,
+    },
+    {
+      image: "Homero.png",
+      category: "Kids",
+      name: "Homero",
+      price: 400,
+      stock: 12,
+      id: 3,
+    },
+    {
+      image: "Patitas.png",
+      category: "Wild",
+      name: "Patitas",
+      price: 400,
+      stock: 10,
+      id: 4,
+    },
+  ];
+
+  const getProducts = () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(productos);
+      }, 2000);
+    });
+  };
+
+  useEffect(() => {
+    getProducts()
+      .then((response) => {
+        setProducts(response);
+      })
+      .catch((err) => {})
+      .finally(() => {});
+  }, []);
   return (
-    <>
-      <ItemList title="Productos Destacados" />
-    </>
+    <div>
+      <ItemList title={"Productos Destacados"} products={products} />
+    </div>
   );
 };
 
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react"
+import ItemListContainer from "../components/ItemList/ItemList"
+
+
+const Home = () => {
+    return (
+
+    )
+}
